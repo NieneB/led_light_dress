4ec8b5f8d3d4bb1907acac5ac047748e0e5782ac
NieneB <niene.boeijen@gmail.com>
2018-02-10 14:18:39 +0100

    more programs

--- my_python/web_app/main.py
+++ my_python/web_app/main.py
@@ -36,6 +36,29 @@ ALL_LED_TOP = strip1 + strip2 + topR + strip3 +strip4 + strip5 + strip6 +strip7+
 ALL_LED = strip1 + strip2 + strip3 +strip4 + strip5 + strip6 +strip7+ strip8 + strip9 +strip10
 ALL_STRIP_MATRIX = [strip1, strip2,strip3,strip4, strip5 ,strip6,strip7,strip8, strip9,strip10]
 
+LOW_LED = strip1[12:15] + strip2[12:15] + strip3[12:15] +strip4[12:15] + strip5[12:15] + strip6[12:15] +strip7[12:15]+ strip8[12:15] + strip9[12:15] +strip10[12:15]
+
+
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbow(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(LED_COUNT):
+            strip.setPixelColor(ALL_LED[i], wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
 def colorWipe(strip,color, wait_ms=80):
     for i in range(0,6):
         strip.setPixelColor(topL[i],color)
@@ -56,6 +79,17 @@ def colorWipe(strip,color, wait_ms=80):
         strip.show()
         time.sleep(wait_ms/1000.0)
 
+def lowEnergy(strip, color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, LED_COUNT, 3):
+                strip.setPixelColor(LOW_LED[i]+q, color)
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, LED_COUNT, 3):
+                strip.setPixelColor(LOW_LED[i]+q, 0)
+
 def oneColor(strip,color):
     for i in range(LED_COUNT):
         strip.setPixelColor(i, color)
@@ -67,7 +101,7 @@ CurrentSignal = "OFF"
 
 def Main():
 	global CurrentSignal
-
+    global LED_COUNT
 	if request.args.get('Signal'):
 		CurrentSignal=request.args.get('Signal')
                 # Setting top aan uit
@@ -82,12 +116,16 @@ def Main():
                     print 'P1'
                     colorWipe(strip,Color(255,0,10))
                     colorWipe(strip,Color(0,0,0))
+                    colorWipe(strip,Color(255,0,10))
+                    colorWipe(strip,Color(0,0,0))
+                    colorWipe(strip,Color(255,0,10))
+                    colorWipe(strip,Color(0,0,0))
                 if CurrentSignal=='P2':
                     print 'P2'
-                    colorWipe(strip,Color(0,0,0))
+                    lowEnergy(strip)
                 if CurrentSignal=='red':
                     print 'P2'
-                    oneColor(strip,Color(255,0,0))
+                    rainbow(strip)
 	return render_template('index.html')
 
 
